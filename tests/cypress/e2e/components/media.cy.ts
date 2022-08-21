5fe0315e8b88d14ca13dd010f4b190a9d4e9477e
Preschian Febryantara <preschian27@gmail.com>
2022-08-21 20:55:03 +0700

    tag related should exists

--- tests/cypress/e2e/components/media.cy.ts
+++ tests/cypress/e2e/components/media.cy.ts
@@ -85,6 +85,7 @@ describe('Media component', () => {
         cy.getCy('item-collection').scrollIntoView().click()
         cy.location('pathname').should('include', '/collection/')
         cy.getCy('small-display').click().scrollIntoView()
+        cy.getCy(tagRelated).should('exist')
         cy.waitForNetworkIdle('*', '*', 1000)
         cy.document().then((doc) => {
           const totalItems = doc.querySelectorAll(
