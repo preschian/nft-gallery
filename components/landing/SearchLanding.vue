fbd29a6036b4126a7bd60a52f9b3179d15b1b11d
Preschian Febryantara <preschian@users.noreply.github.com>
2023-01-17 18:05:54 +0700

    test

--- components/landing/SearchLanding.vue
+++ components/landing/SearchLanding.vue
@@ -3,29 +3,35 @@
     <img src="/landing-blurred-header-left.svg" class="landing-search-left" />
     <img :src="landingImage[0]" class="landing-shapes" />
     <div
-      class="is-flex is-flex-direction-column is-align-items-center search-info">
+      class="is-flex is-flex-direction-column is-align-items-center search-info"
+    >
       <h1
-        class="title is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered">
-        <span>{{ $t('search.landingTitle1') }}</span>
+        class="title is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered"
+      >
+        <span>{{ $t('search.landingTitle1') }} 18.04</span>
         {{ $t('search.landingTitle2') }}
         <span> {{ $t('search.landingTitle3') }}</span>
       </h1>
       <span
-        class="subtitle is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered is-capitalized">
+        class="subtitle is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered is-capitalized"
+      >
         {{ $t('search.landingSubtitle') }}
       </span>
       <LazySearch
         hide-filter
         class="landing-search-bar"
-        search-column-class="is-flex-grow-1" />
+        search-column-class="is-flex-grow-1"
+      />
       <div
         id="networkList"
-        class="is-flex is-justify-content-center is-flex-wrap-wrap is-align-items-baseline">
+        class="is-flex is-justify-content-center is-flex-wrap-wrap is-align-items-baseline"
+      >
         <a
           v-for="chain in chainList"
           :key="chain.value"
           :class="['m-2', 'chain-option active']"
-          @click="switchChain(chain.value)">
+          @click="switchChain(chain.value)"
+        >
           {{ chainText(chain.text) }}
           <sup v-if="chain.text.includes('[Beta]')" class="beta-text is-size-7">
             {{ $t('beta') }}</sup
@@ -39,51 +45,51 @@
 </template>
 
 <script lang="ts" setup>
-import { Option } from '@kodadot1/vuex-options/dist/types'
+import { Option } from '@kodadot1/vuex-options/dist/types';
 
-import { getChainTestList } from '~/utils/constants'
+import { getChainTestList } from '~/utils/constants';
 
-const { urlPrefix } = usePrefix()
-const { $store, $colorMode, $router } = useNuxtApp()
+const { urlPrefix } = usePrefix();
+const { $store, $colorMode, $router } = useNuxtApp();
 const isDarkMode = computed(
   () =>
     $colorMode.preference === 'dark' ||
-    document.documentElement.className.includes('dark-mode')
-)
+    document.documentElement.className.includes('dark-mode'),
+);
 
 const chainText = (chain: string) => {
   if (chain.includes('[Beta]')) {
-    return chain.split(' ')[0]
+    return chain.split(' ')[0];
   } else {
-    return chain
+    return chain;
   }
-}
+};
 const landingImage = computed(() => {
   if (isDarkMode.value) {
     return [
       '/landing-shape-header-left-dark.svg',
       '/landing-shape-header-right-dark.svg',
-    ]
+    ];
   } else {
     return [
       '/landing-shape-header-left-light.svg',
       '/landing-shape-header-right-light.svg',
-    ]
+    ];
   }
-})
+});
 
 const chainList = computed(() => {
-  const availableUrlPrefixes: Option[] = $store.getters['availableUrlPrefixes']
+  const availableUrlPrefixes: Option[] = $store.getters['availableUrlPrefixes'];
   return availableUrlPrefixes.filter(
-    (urlPrefix) => !getChainTestList().includes(urlPrefix.value as string)
-  )
-})
+    (urlPrefix) => !getChainTestList().includes(urlPrefix.value as string),
+  );
+});
 
 const switchChain = (value) => {
   if (value === urlPrefix.value) {
-    return
+    return;
   }
-  $store.dispatch('setUrlPrefix', value)
-  $router.push({ path: `/${value}/explore/collectibles` })
-}
+  $store.dispatch('setUrlPrefix', value);
+  $router.push({ path: `/${value}/explore/collectibles` });
+};
 </script>
