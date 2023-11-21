1c5fc1bba9b1c830b9112c8c4c7923d738920386
Preschian Febryantara <preschian@users.noreply.github.com>
2023-11-22 05:04:12 +0700

    Add middleware to disable SSR for blog routes

---
+++ server/middleware/disable-ssr.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler((event) => {
+  console.log('event', event.path)
+
+  if (!event.path.includes('/blog')) {
+    return
+  }
+
+  event.context.nuxt = event.context.nuxt || {}
+  event.context.nuxt.noSSR = true
+})
